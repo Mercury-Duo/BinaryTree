835fdcca4e7afe1482bda7f6c9ff4c5f880d12fd
铎 <@DESKTOP-733MSEH>
2021-05-06 20:35:52 +0800

    添加项目文件。

---
+++ 二叉树/二叉树.cpp
@@ -0,0 +1,40 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+typedef struct TREE {
+	int data;
+	struct TREE *Ltree;
+	struct TREE* Rtree;
+}TREE,*LPTREE;
+struct TREE* CreatTree(int data) {
+     LPTREE mytree=(LPTREE)malloc(sizeof(TREE));
+	 mytree->data = data;
+	 mytree->Ltree = NULL;
+	 mytree->Rtree = NULL;
+	 return mytree;
+}
+void PrintTree(LPTREE data)
+{
+	printf("%d",data->data);
+}
+void Printt1(LPTREE mytree)
+{
+	if (mytree != NULL)
+	{
+		printf("%d",mytree->data);
+		Printt1(mytree->Ltree);
+		Printt1(mytree->Rtree);
+	}
+
+}
+int main(void)
+{
+	LPTREE s1 = CreatTree(1);
+	LPTREE s2 = CreatTree(2);
+	LPTREE s3 = CreatTree(3);
+	LPTREE s4 = CreatTree(4);
+	LPTREE s5 = CreatTree(5);
+	s1->Ltree = s2;s1->Rtree = s3;
+	s2->Ltree = s4;s2->Rtree = s5;
+	Printt1(s1);
+	system("pause");
+}
